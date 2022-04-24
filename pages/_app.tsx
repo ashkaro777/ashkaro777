9af816b3f2700318c59298f328720762f4cb1c97
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2022-04-25 02:03:33 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/reset.css";
+import "../styles/bootstrap.min.css";
+import "../styles/color.css";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from "next/app";
+import AppLayout from "common/components/AppLayout";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  );
 }
 
-export default MyApp
+export default App;
