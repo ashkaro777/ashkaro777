28bc2e44e39da4c7bf156b38ae3cde5722b5f401
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2022-04-25 02:04:55 +0530

    Update button nav panel

---
+++ common/components/ButtonNavPanel/components/NavItem.tsx
@@ -0,0 +1,82 @@
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import Image from "next/image";
+
+import { squareSizing } from "common/utils/responsive/sizingUtils";
+import { screenSmallerThan } from "common/utils/responsive";
+
+const StyledImage = styled(Image)`
+  ${squareSizing("26px", "--icon-size")}
+  border-radius: inherit;
+`;
+
+const StyledNavItem = styled.a<{ $isActive?: boolean; $color?: string }>`
+  --active-size: 80px;
+  ${squareSizing("18px")}
+
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${(props) => props.$color || "#fff"};
+  border-radius: 777rem;
+  cursor: pointer;
+  border: 2px solid #fff;
+
+  ${screenSmallerThan.tablet} {
+    --active-size: 48px;
+    --size: 10px;
+  }
+
+  & > * {
+    transition: filter 240ms ease-in-out;
+  }
+
+  ${(props) =>
+    props.$isActive
+      ? `
+            --size: var(--active-size) !important; 
+            transition: all 240ms ease-in-out 120ms;
+
+            & > * {
+                filter: opacity(1);
+                transition: filter 0ms;
+            }
+        `
+      : `
+            transition: all 400ms ease-in-out;
+
+            & > * {
+                filter: opacity(0);
+                transition: filter 240ms ease-in-out;
+            }
+        `}
+`;
+
+const NavItem: React.FunctionComponent<{
+  id: string;
+  label: string;
+  icon: string;
+  color?: string;
+  className?: string;
+  isActive?: boolean;
+  onClick?: (id: string) => void;
+}> = ({ className, label, onClick, icon, id, isActive, color }) => {
+  const handleClick = useCallback(() => {
+    if (onClick) onClick(id);
+  }, [onClick, id]);
+
+  return (
+    <StyledNavItem
+      $isActive={isActive}
+      $color={color}
+      className={className}
+      onClick={handleClick}
+      href={`#${id}`}
+    >
+      <StyledImage src={icon} alt={label} />
+    </StyledNavItem>
+  );
+};
+
+export default NavItem;
