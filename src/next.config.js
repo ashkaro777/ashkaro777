38ccf1369cf4cca6a176587662819d15fdc7d387
lazy-manushya <anukesh.bhawsar@outlook.com>
2022-06-12 22:11:59 +0530

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/ashkaro777/ashkaro777.github.io/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/ashkaro777/ashkaro777.github.io/ashkaro777/' : '',
 }
 
 module.exports = nextConfig
