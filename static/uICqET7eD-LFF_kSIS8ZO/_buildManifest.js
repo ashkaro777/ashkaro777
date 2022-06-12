8c70d555d322256df4aadfea68cc2852f0d853dc
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2022-06-12 16:38:50 +0000

    Deploying to gh-pages from  @ 0e291606684593a0ff0257d8920ec659591857b2 🚀

---
+++ static/uICqET7eD-LFF_kSIS8ZO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/685-16735da697702ac4.js","static/chunks/pages/index-12503fa17f0d769e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
