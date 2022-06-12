8d2fcc00f4d905e986f97981ad44fbdbedc3d908
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2022-06-12 16:35:30 +0000

    Deploying to gh-pages from  @ c52925fe19e9d0cfc978ff03b926d706fca22265 🚀

---
+++ static/EqM_QBVxb2ZxUoIPzmuNi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/685-16735da697702ac4.js","static/chunks/pages/index-f4b9ac90a9ba3cc2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- static/pfAcSNAqAX0T0ALuw8PMf/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/685-16735da697702ac4.js","static/chunks/pages/index-ebc4db0c1241c1f2.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
