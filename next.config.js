806ec31274070afd9403d3b568c4c03ecaa2a7a6
Dewashish <dewashishbhawsar99@gmail.com>
2022-06-12 21:16:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
 }
 
 module.exports = nextConfig
