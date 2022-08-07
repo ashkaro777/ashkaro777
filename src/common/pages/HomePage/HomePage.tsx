95c4201b556f60f783a2f3776853ee21a1950f8c
lazy-manushya <anukesh.bhawsar@outlook.com>
2022-08-08 01:44:12 +0530

    ADD - Home page

--- src/common/pages/HomePage/HomePage.tsx
+++ src/common/pages/HomePage/HomePage.tsx
@@ -2,19 +2,17 @@ import React from "react";
 
 import HeroSection from "./components/HeroSection";
 import ButtonNav from "./components/ButtonNav";
-import BookDetailsSection from "common/components/BookDetailsSection";
 import TravelSection from "common/components/TravelSection";
 import PhotographySection from "common/components/PhotographySection";
-import { StyledHomePage } from "./styles";
-import BookSection from "../BookSection";
+import BookDetailsSection from "common/components/book/BookDetailsSection";
+import { StyledHomePage } from "./styles"; 
 
 const HomePage: React.FunctionComponent = () => {
   return (
     <StyledHomePage>
       <ButtonNav />
       <HeroSection />
-      <BookDetailsSection />
-      <BookSection />
+      <BookDetailsSection /> 
       <TravelSection />
       <PhotographySection />
     </StyledHomePage>
