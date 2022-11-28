06bf42b6f2a78c4d445cc068e15c811815b28e8d
lazy-manushya <lazy-manushya@gmail.com>
2022-11-28 15:04:00 +0530

    Update HeroSection.tsx

--- src/common/pages/HomePage/components/HeroSection.tsx
+++ src/common/pages/HomePage/components/HeroSection.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import styled from "styled-components";
 
 import { ELEMENT_ID } from "common/constants/element";
 import { IHeroSectionProps } from "../interfaces";
 import { StyledHeading, StyledHeroSection } from "../styles";
 
+const StypedBackgroundVideo = styled.video`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  z-index: 0;
+`;
+
 const HeroSection: React.FunctionComponent<IHeroSectionProps> = () => {
   return (
     <StyledHeroSection id={ELEMENT_ID.HERO_SECTION}>
       <StyledHeading>Ashish Jahangid</StyledHeading>
+      <StypedBackgroundVideo
+        src={"./assets/videos/dance-grains.mp4"}
+        autoPlay
+        loop
+        muted
+      />
     </StyledHeroSection>
   );
 };
