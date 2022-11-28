1e8779e129509a984393e3d40dd988ae1597fe49
lazy-manushya <lazy-manushya@gmail.com>
2022-11-28 15:04:02 +0530

    Update HomePage.tsx

--- src/common/pages/HomePage/HomePage.tsx
+++ src/common/pages/HomePage/HomePage.tsx
@@ -5,16 +5,16 @@ import ButtonNav from "./components/ButtonNav";
 import TravelSection from "common/components/TravelSection";
 import PhotographySection from "common/components/PhotographySection";
 import BookDetailsSection from "common/components/book/BookDetailsSection";
-import { StyledHomePage } from "./styles"; 
+import { StyledHomePage } from "./styles";
 
 const HomePage: React.FunctionComponent = () => {
   return (
     <StyledHomePage>
       <ButtonNav />
       <HeroSection />
-      <BookDetailsSection /> 
-      <TravelSection />
-      <PhotographySection />
+      <BookDetailsSection />
+      {false && <TravelSection />}
+      {false && <PhotographySection />}
     </StyledHomePage>
   );
 };
