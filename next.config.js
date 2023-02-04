34363b2bd05a191b4a6a8c5435edb05f3707a248
lazy-manushya <lazy-manushya@gmail.com>
2023-02-04 16:57:07 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/ashkaro777/ashkaro777.github.io/gh-pages/' : '',
-}
-
-module.exports = nextConfig
